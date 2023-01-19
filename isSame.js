e9ed881d235c8a76fad692e4d20acdd320157d83
kethnal <kaseyvaldez@hotmail.com>
2023-01-19 00:00:44 -0800

    Refactor isSame

--- isSame.js
+++ isSame.js
@@ -15,27 +15,50 @@
 // b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
 // comp(a,b) returns false because in b 132 is not the square of any number of a.
 
-function comp(array1, array2){
-  if (array1.length === 0 || array2.length === 0 || array1 === null || array2 === null) {
-    return false;
-  }
+// function comp(array1, array2){
+//   if (array1 === null || array2 === null) return false;
+//   if (array1.length === 0 && array1.length === array2.length) return false;
+    
+//   for (let i = 0; i < array1.length; i++) {
+//     for (let j = 0; j < array1.length; j++) {
+//       if (array1[i] * array1[i] === array2[j]) {
+//         array2.splice(j, 1);
+//         console.log("array2: ", array2);
+//       }
+//     }
+//   }
   
-  let output = [];
-  
-  for (let num1 of array1) {
-    for (let num2 of array2) {
-      if (num1*num1 === num2 || num2*num2 === num1) {
-        output.push(num1);
-        break;
-      }
-    }
-  }
-  
-  console.log(output)
-  return output.length === array1.length;
-}
+//   return array2.length === 0;
+// }
 
 let a1 = [121, 144, 19, 161, 19, 144, 19, 11];
 let a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19];
 
+let b2 = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+
 console.log(comp(a1, a2)); // should be true
+console.log(comp(a1, b2)); // should be false
+
+// above doesn't pass all tests
+
+// REFACTORED
+
+function comp(array1, array2){
+  if (array1 === null || array2 === null) return false;
+  if (array1.length === 0 && array1.length !== array2.length) return false;
+    
+  const squaredArray = array1.map(num => num * num);
+  
+  const sortNumbers = (a, b) => {
+    return a - b;
+  }
+
+  const newArray1 = squaredArray.sort(sortNumbers);
+  const newArray2 = array2.sort(sortNumbers);
+  
+  for (let i = 0; i < newArray1.length; i++) {
+    if (newArray1[i] !== newArray2[i]) return false;
+  }
+
+  return true;
+}
